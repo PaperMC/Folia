300f1c723e8a940297cdf9ce908a122ce6525e14
MiniDigger <admin@minidigger.me>
2021-06-12 14:20:45 +0200

    add repos

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,6 +17,28 @@ allprojects {
     }
 }
 
+subprojects {
+    tasks.withType<JavaCompile>().configureEach {
+        options.encoding = "UTF-8"
+        options.release.set(16)
+    }
+
+    if (name == "Paper-MojangAPI") {
+        return@subprojects
+    }
+
+    repositories {
+        mavenCentral()
+        maven("https://repo1.maven.org/maven2/")
+        maven("https://oss.sonatype.org/content/groups/public/")
+        maven("https://papermc.io/repo/repository/maven-public/")
+        maven("https://ci.emc.gs/nexus/content/groups/aikar/")
+        maven("https://repo.aikar.co/content/groups/aikar")
+        maven("https://repo.md-5.net/content/repositories/releases/")
+        maven("https://hub.spigotmc.org/nexus/content/groups/public/")
+    }
+}
+
 dependencies {
     implementation(gradleApi())
 }
