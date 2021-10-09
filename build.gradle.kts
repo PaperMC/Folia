e0a85eb4c9d2488289bcda1276332f70b2453ea5
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-10-08 22:38:07 -0700

    Update shadow to 7.1.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import io.papermc.paperweight.util.constants.*
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "7.0.0" apply false
+    id("com.github.johnrengelman.shadow") version "7.1.0" apply false
     id("io.papermc.paperweight.patcher") version "1.1.12"
 }
 
