c197882df11d68d289307c2bd33782f92972e80c
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2021-12-23 19:56:45 -0800

    update paperweight, gradle wrapper, and shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@ import io.papermc.paperweight.util.constants.*
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "7.1.0" apply false
-    id("io.papermc.paperweight.patcher") version "1.3.1"
+    id("com.github.johnrengelman.shadow") version "7.1.1" apply false
+    id("io.papermc.paperweight.patcher") version "1.3.3"
 }
 
 repositories {
