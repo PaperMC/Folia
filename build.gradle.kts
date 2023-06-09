1b0a5071d06c2d380cd63492937ba4a8b1030c16
Spottedleaf <Spottedleaf@users.noreply.github.com>
2023-06-08 20:15:55 -0700

    Initial patch apply

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import io.papermc.paperweight.tasks.RebuildGitPatches
 plugins {
     java
     `maven-publish`
-    id("com.github.johnrengelman.shadow") version "8.1.0" apply false
+    id("com.github.johnrengelman.shadow") version "8.1.1" apply false
     id("io.papermc.paperweight.patcher") version "1.5.5"
 }
 
