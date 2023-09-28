cca4fd2acec3a9abe7ad324ede49c29de4ba6113
Yomamaeatstoes <114538961+Yomamaeatstoes@users.noreply.github.com>
2023-09-28 17:32:31 -0400

    Update build.gradle.kts - Fix again

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
     java
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "8.1.1" apply false
-    id("io.papermc.paperweight.patcher") version "1.5.7"
+    id("io.papermc.paperweight.patcher") version "1.5.7-SNAPSHOT"
 }
 
 val paperMavenPublicUrl = "https://repo.papermc.io/repository/maven-public/"
