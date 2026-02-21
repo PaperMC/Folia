0f40775107a30d1947975b8278ff48abed885658
tksimeji <tksimeji@outlook.com>
2026-02-21 20:09:33 +0900

    build: add the jvm version specification to plugins build script

--- plugins/build.gradle.kts
+++ plugins/build.gradle.kts
@@ -1,11 +1,17 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+import org.gradle.jvm.toolchain.JavaLanguageVersion
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+
 plugins {
     java
     kotlin("jvm") version "2.3.0" apply false
+    id("com.gradleup.shadow") version "9.3.1" apply false
 }
 
 subprojects {
     apply(plugin = "java-library")
     apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "com.gradleup.shadow")
 
     repositories {
         mavenCentral()
@@ -13,14 +19,27 @@ subprojects {
     }
 
     dependencies {
-        "compileOnly"("dev.folia:folia-api:1.21.11-R0.1-SNAPSHOT")
-        "implementation"(kotlin("stdlib"))
+        compileOnly("dev.folia:folia-api:1.21.11-R0.1-SNAPSHOT")
+        implementation(kotlin("stdlib"))
+    }
+
+    extensions.configure<JavaPluginExtension> {
+        toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+    }
+    extensions.configure<KotlinJvmProjectExtension> {
+        jvmToolchain(21)
+    }
+
+    tasks.named<ShadowJar>("shadowJar") {
+        archiveClassifier.set("")
+    }
+    tasks.named<Jar>("jar") {
+        enabled = false
     }
 }
 
 tasks.register("buildAllPluginJars") {
     group = "build"
-    description = "Build JARs for all plugin subprojects"
-
-    dependsOn(subprojects.map { it.tasks.named<Jar>("jar") })
+    description = "Build shadow JARs for all plugin subprojects"
+    dependsOn(subprojects.map { it.tasks.named("shadowJar") })
 }
--- plugins/gameplay/src/main/kotlin/net/azisaba/aetheria/plugins/gameplay/Main.kt
+++
@@ -1,6 +0,0 @@
-package net.azisaba.aetheria.plugins.gameplay
-
-import org.bukkit.plugin.java.JavaPlugin
-
-class Main : JavaPlugin() {
-}
