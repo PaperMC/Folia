414492e1450cd41b7817c4f62a7fc9918d96da17
sysnote8main <sysnote8main@gmail.com>
2026-03-16 20:54:52 +0900

    feat(islands): create portals on ignite and teleport on portal entry; add metadata, listener, cooldown and logging

--- plugins/plugin-islands/src/main/kotlin/net/azisaba/vanilife/islands/portal/listener/PortalIgniteListener.kt
+++ plugins/plugin-islands/src/main/kotlin/net/azisaba/vanilife/islands/portal/listener/PortalIgniteListener.kt
@@ -1,6 +1,7 @@
 package net.azisaba.vanilife.islands.portal.listener
 
 import com.github.shynixn.mccoroutine.folia.launch
+import com.github.shynixn.mccoroutine.folia.regionDispatcher
 import net.azisaba.vanilife.islands.IslandPos
 import net.azisaba.vanilife.islands.portal.ResourcePortals
 import net.azisaba.vanilife.islands.portal.ResourceTeleporter
