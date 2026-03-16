852933d4fe2d30b732d16ca66eded447a8d62df5
sysnote8main <sysnote8main@gmail.com>
2026-03-16 20:53:20 +0900

    feat(islands): create portals on ignite and teleport on portal entry; add metadata, listener, cooldown and logging

--- plugins/plugin-islands/src/main/kotlin/net/azisaba/vanilife/islands/EventListeners.kt
+++ plugins/plugin-islands/src/main/kotlin/net/azisaba/vanilife/islands/EventListeners.kt
@@ -3,10 +3,12 @@ package net.azisaba.vanilife.islands
 import net.azisaba.vanilife.islands.listener.IslandPlayerListener
 import net.azisaba.vanilife.islands.portal.listener.BedTrackerListener
 import net.azisaba.vanilife.islands.portal.listener.PortalIgniteListener
+import net.azisaba.vanilife.islands.portal.listener.PortalEnterListener
 import org.koin.core.Koin
 
 internal fun Main.setupEventListeners(koin: Koin) {
     server.pluginManager.registerEvents(IslandPlayerListener(koin.get(), koin.get()), this)
     server.pluginManager.registerEvents(BedTrackerListener(this, koin.get(), koin.get<Config>().portal.resourceWorld), this)
     server.pluginManager.registerEvents(PortalIgniteListener(this, koin.get<Config>().portal.resourceWorld, koin.get()), this)
+    server.pluginManager.registerEvents(PortalEnterListener(this, koin.get<Config>().portal.resourceWorld, koin.get()), this)
 }
