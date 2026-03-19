d4a602fbfccfc2d9940d62f2af12e36e4dd5371a
sysnote8main <sysnote8main@gmail.com>
2026-03-19 20:42:43 +0900

    feat: remove unused module

--- plugins/plugin-portal/src/main/kotlin/net/azisaba/vanilife/portal/Main.kt
+++
@@ -1,9 +0,0 @@
-package net.azisaba.vanilife.portal
-
-import org.bukkit.plugin.java.JavaPlugin
-
-class Main : JavaPlugin() {
-    override fun onEnable() {
-        setupEventListeners()
-    }
-}
