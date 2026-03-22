6d074dafb297f377ec4e4bd6ba8e10f97c57176b
tksimeji <tksimeji@outlook.com>
2026-03-22 11:27:31 +0900

    remove: remove old portal implementation

--- plugins/plugin-islands/src/main/kotlin/net/azisaba/vanilife/islands/Database.kt
+++ plugins/plugin-islands/src/main/kotlin/net/azisaba/vanilife/islands/Database.kt
@@ -2,8 +2,6 @@ package net.azisaba.vanilife.islands
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
-import net.azisaba.vanilife.islands.portal.LastBedTable
-import net.azisaba.vanilife.islands.portal.ResourceSpawnTable
 import net.azisaba.vanilife.islands.repository.DatabaseIslandRepository
 import org.jetbrains.exposed.v1.jdbc.Database
 import org.jetbrains.exposed.v1.jdbc.SchemaUtils
@@ -27,8 +25,6 @@ internal fun Main.setupDatabase(databaseConfig: DatabaseConfig): Database {
 internal fun Database.setupTables(): Database = transaction(this) {
     SchemaUtils.create(
         DatabaseIslandRepository.IslandsTable,
-        ResourceSpawnTable,
-        LastBedTable,
     )
     this@setupTables
 }
